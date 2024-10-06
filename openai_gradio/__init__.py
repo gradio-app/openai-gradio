2b511ebbf08174f4103b13f74ff48a07e4273c59
AK391 <ahsen@huggingface.co>
2024-10-06 01:04:54 -0400

    update version

--- openai_gradio/__init__.py
+++ openai_gradio/__init__.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 import gradio as gr
 from typing import Callable
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 def get_fn(model_name: str, preprocess: Callable, postprocess: Callable, api_key: str):
